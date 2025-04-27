56ce6fa195b33732684747eefe4941b6f4e49e18
Archie Agarwal <archieagarwal2001@gmail.com>
2025-04-27 17:46:22 +0530

    basic backend created

--- serverApp.js
+++ serverApp.js
@@ -2,18 +2,29 @@ import express from "express";
 import dotenv from "dotenv";
 import { fileURLToPath } from "url";
 import path from "path";
+import fs from "fs"
 dotenv.config();
 let app=express();
 let filename=fileURLToPath(import.meta.url);
 let dirname=path.dirname(filename);
+let originalLink, shortName, pathFile;
 app.use(express.static(path.join(dirname, "../../frontend/react-app/dist")));
 app.use(express.urlencoded());
 export let PORT=process.env.PORT || 5000;
-app.get("/submit", (req, resp)=>{
-    console.log(resp);
-});
+
+app.get("/submit", async (req, resp)=>{
+    pathFile=`http://localhost:${PORT}//${shortName}`;
+    resp.send(pathFile);
+    
+})
+app.get("//:shortName", (req, resp)=>{
+    resp.redirect(originalLink);
+})
+
 app.post("/", async (req, resp)=>{
-    console.log(req.body); 
+    originalLink=req.body.originalLink;
+    shortName=req.body.shortName;
+    resp.redirect(`/submit`);
     
 })
 app.listen(PORT, ()=>{
