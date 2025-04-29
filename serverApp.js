412ceef34bfdd9ebced3fa5762a85972807ac2b6
Archie Agarwal <archieagarwal2001@gmail.com>
2025-04-30 00:30:48 +0530

    database cfeation started

--- serverApp.js
+++ serverApp.js
@@ -2,7 +2,9 @@ import express from "express";
 import dotenv from "dotenv";
 import { fileURLToPath } from "url";
 import path from "path";
-import fs from "fs"
+import fs from "fs";
+import data from "./data.json" assert { type: "json" };
+import assert from "assert";
 dotenv.config();
 let app=express();
 let filename=fileURLToPath(import.meta.url);
@@ -15,9 +17,19 @@ export let PORT=process.env.PORT || 5000;
 app.get("/submit", async (req, resp)=>{
     pathFile=`http://localhost:${PORT}//${shortName}`;
     resp.send(pathFile);
+    data.originalLink=originalLink;
+    data.shortName=shortName;
+    data.shortenedLink=pathFile;
+    fs.writeFile("./data.json", JSON.stringify(data), (err)=>{
+        if(err){
+
+        }else{
+            
+        }
+    })  
+})  
     
-})
-app.get("//:shortName", (req, resp)=>{
+ app.get("//:shortName", (req, resp)=>{
     resp.redirect(originalLink);
 })
 
